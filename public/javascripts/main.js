7fdb90d5d088ee8e93f6b317e8d680be3d32b8f3
Luke Stebner <luke.stebner@gmail.com>
2014-05-18 19:18:42 -0700

    bower_components added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,10 @@
-//@codekit-prepend "../components/jquery/jquery.js";
-//@codekit-prepend "underscore.js";
+//@codekit-prepend "../bower_components/jquery/dist/jquery.js";
+//@codekit-prepend "../bower_components/underscore/underscore.js";
 //@codekit-prepend "css3-mediaqueries.js";
 
-$(function(){
-    //a resize function which adjusts the header background image so that it stays properly centered
-    $(window).resize(function(){
-        if ($(window).width() < 2000){
-            var diff = 2000 - $(window).width();
-            $('#header').css('backgroundPosition', Math.round(diff/2) * -1);
-        }
-    }).resize();
+World = {}
 
+$(function(){
     //check all links for domain and if it's not relative, then make it target:_blank
     $('a').each(function(){
         if ($(this).attr('href').search(new RegExp('http(|s):\/\/')) > -1){
